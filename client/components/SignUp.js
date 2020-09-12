0f652be91be44969d39d5ce6f345fc3cd537d9ff
“StefanPougatchev” <Stefan.Pougatchev@gmail.com>
2020-09-12 13:45:24 -0700

    added some file structure to the signup page

---
+++ client/components/SignUp.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+const Join = () => {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
+  return <div></div>;
+};
--- server/server.js
+++ server/server.js
@@ -1,20 +1,28 @@
-const express = require('express');
-const path = require('path');
-const PORT = 3000;
+const express = require("express");
+const path = require("path");
 const app = express();
+const http = require("http");
+const socketio = require("socket.io");
+const server = http.createServer(app);
+const io = socketio(server);
+const PORT = 3000;
 
-app.use('/build', express.static(path.join(__dirname, '../build')));
+app.use("/build", express.static(path.join(__dirname, "../build")));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../index.html"));
 });
 
-app.use('*', (err, req, res, next) => {
-  res.status(404).send(err);
-})
+io.on("connection", () => {
+  console.log("user has connnected");
+});
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`)
+// app.use("*", (err, req, res, next) => {
+//   res.status(404).send(err);
+// });
+
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
 
 module.exports = app;
